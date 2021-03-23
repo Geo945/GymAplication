fc5213f7dde30d7b984be21ec6b49b5e884fcf90
Geo <georgeX_D@yahoo.com>
2021-03-23 15:12:13 +0200

    maven, gradle files

--- src/proiect/fis/gym/aplication/Main.java
+++ src/proiect/fis/gym/aplication/Main.java
@@ -1,11 +1,9 @@
 package proiect.fis.gym.aplication;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
